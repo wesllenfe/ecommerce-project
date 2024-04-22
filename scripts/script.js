18da0b45937ad4a72d38be9a2088f52ac3b1a8f9
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-04-21 22:13:10 -0300

    feat: adicionando funcao de calcular valores no carrinho

--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,57 @@ cartIcon.onclick = () => {
 closeCart.onclick = () => {
   cart.classList.remove("active");
 }
+
+
+if(document.readyState == 'loading'){
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready();
+}
+
+function ready() {
+  var removeCartButtons = document.getElementsByClassName('cart-remove');
+  console.log(removeCartButtons);
+  for(var i = 0; i < removeCartButtons.length; i++){
+    var button = removeCartButtons[i];
+    button.addEventListener('click', removeCartItem);
+  }
+  var quantityInputs = document.getElementsByClassName('cart-quantity');
+  for(var i = 0; i < quantityInputs.length; i++){
+    var input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+}
+
+function removeCartItem(event) {
+  var buttonClicked = event.target;
+  buttonClicked.parentElement.remove();
+  updateTotal();
+}
+
+function quantityChanged(event) {
+  var input = event.target;
+  if (isNaN(input.value) || input.value <= 0) {
+    input.value = 1;
+  }
+  updateTotal();
+}
+
+function updateTotal(){
+  var cartBoxes = document.getElementsByClassName('cart-box');
+  var total = 0;
+  for(var i = 0; i < cartBoxes.length; i++){
+    var cartBox = cartBoxes[i];
+    var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+    var price = parseFloat(priceElement.innerText.replace('$', '').trim());
+    var quantity = parseInt(quantityElement.value);
+    if (!isNaN(price) && !isNaN(quantity)) {
+      total += price * quantity;
+
+      total = Math.round(total * 100) / 100;
+    }
+  }
+  document.querySelector('.total-price').innerText = '$' + total.toFixed(2);
+}
+updateTotal();
