a7fefef35ccb3275e7391440d3161471b210bdc2
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-04-22 10:30:41 -0300

    feat: adicionado responsividade e mais funcionalidade aos botoes

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,6 @@ if(document.readyState == 'loading'){
 
 function ready() {
   var removeCartButtons = document.getElementsByClassName('cart-remove');
-  console.log(removeCartButtons);
   for(var i = 0; i < removeCartButtons.length; i++){
     var button = removeCartButtons[i];
     button.addEventListener('click', removeCartItem);
@@ -29,6 +28,21 @@ function ready() {
     var input = quantityInputs[i];
     input.addEventListener("change", quantityChanged);
   }
+  var addCart = document.getElementsByClassName('add-cart');
+  for(var i = 0; i < addCart.length; i++){
+    var button = addCart[i];
+    button.addEventListener('click', addCartClicked);
+  }
+  document.getElementsByClassName('btn-buy')[0].addEventListener('click', buyButtonClicked);
+}
+
+function buyButtonClicked() {
+  alert("Your order is placed");
+  var cartContent = document.getElementsByClassName('cart-content')[0];
+  while (cartContent.hasChildNodes()) {
+    cartContent.removeChild(cartContent.firstChild);
+  }
+  updateTotal();
 }
 
 function removeCartItem(event) {
@@ -45,6 +59,46 @@ function quantityChanged(event) {
   updateTotal();
 }
 
+function addCartClicked(event) {
+  var button = event.target;
+  var shopProduct = button.parentElement;
+  var title = shopProduct.querySelector('.product-title').innerText;
+  var price = shopProduct.querySelector('.price').innerText;
+  var productImg = shopProduct.querySelector('.produt-img').src;
+  addProductToCart(title, price, productImg);
+  updateTotal();
+}
+
+function addProductToCart(title, price, productImg) {
+  var cartItems = document.querySelector(".cart-content");
+  var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+  for (var i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText === title) {
+      alert("You have already added this item to cart");
+      return; 
+    }
+  }
+
+  var cartShopBox = document.createElement("div");
+  cartShopBox.classList.add('cart-box');
+  var cartBoxContent = `
+    <img src="${productImg}" alt="" class="cart-img">
+    <div class="detail-box">
+      <div class="cart-product-title">${title}</div>
+      <div class="cart-price">${price}</div>
+      <input type="number" value="1" class="cart-quantity">
+    </div>
+    <i class='bx bxs-trash-alt cart-remove'></i>
+  `;
+  cartShopBox.innerHTML = cartBoxContent;
+  cartItems.appendChild(cartShopBox);
+
+  cartShopBox.querySelector('.cart-remove').addEventListener('click', removeCartItem);
+  cartShopBox.querySelector('.cart-quantity').addEventListener('change', quantityChanged);
+
+  updateTotal();
+}
+
 function updateTotal(){
   var cartBoxes = document.getElementsByClassName('cart-box');
   var total = 0;
@@ -56,10 +110,8 @@ function updateTotal(){
     var quantity = parseInt(quantityElement.value);
     if (!isNaN(price) && !isNaN(quantity)) {
       total += price * quantity;
-
       total = Math.round(total * 100) / 100;
     }
   }
   document.querySelector('.total-price').innerText = '$' + total.toFixed(2);
 }
-updateTotal();
